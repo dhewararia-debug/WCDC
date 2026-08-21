307d94e5e48e9b05ef91cd4ea29bce9ea51491b7
dhewararia-debug <dhewararia@gmail.com>
2026-08-21 12:59:46 +0530

    ok
    ok

---
+++ krutidev.js
@@ -0,0 +1,142 @@
+var kruti_array = new Array("Ã±","Q+Z","sas","aa",")Z","ZZ","â€˜","â€™","â€œ","â€","Ã¥",  "Æ’",  "â€ž",   "â€¦",   "â€ ",   "â€¡",   "Ë†",   "â€°",   "Å ",   "â€¹", "Â¶+",   "d+", "[+k","[+", "x+",  "T+",  "t+", "M+", "<+", "Q+", ";+", "j+", "u+","Ã™k", "Ã™", "Ã¤", "â€“", "â€”","Ã©","â„¢","=kk","f=k",  "Ã ",   "Ã¡",    "Ã¢",   "Ã£",   "Âºz",  "Âº",   "Ã­", "{k", "{", "=",  "Â«", "NÃ®",   "VÃ®",    "BÃ®",   "MÃ®",   "<Ã®", "|", "K", "}","J",   "VÂª",   "MÂª",  "<ÂªÂª",  "NÂª",   "Ã˜",  "Ã", "nzZ",  "Ã¦", "Ã§", "Ã", "xz", "#", ":", "vâ€š","vks",  "vkS",  "vk",    "v",  "bÂ±", "Ãƒ",  "bZ",  "b",  "m",  "Ã…",  ",s",  ",",   "_","Ã´",  "d", "Dk", "D", "[k", "[", "x","Xk", "X", "Ã„", "?k", "?",   "Â³", "pkS",  "p", "Pk", "P",  "N",  "t", "Tk", "T",  ">", "Ã·", "Â¥","Ãª",  "Ã«",   "V",  "B",   "Ã¬",   "Ã¯", "M+", "<+", "M",  "<", ".k", ".", "r",  "Rk", "R",   "Fk", "F",  ")", "n", "/k", "Ã¨k",  "/", "Ã‹", "Ã¨", "u", "Uk", "U",   "i",  "Ik", "I",   "Q",    "Â¶",  "c", "Ck",  "C",  "Hk",  "H", "e", "Ek",  "E", ";",  "Â¸",   "j",    "y", "Yk",  "Y",  "G",  "o", "Ok", "O", "'k", "'",   "\"k",  "\"",  "l", "Lk",  "L",   "g",  "Ãˆ", "z", "ÃŒ", "Ã", "ÃŽ",  "Ã",  "Ã‘",  "Ã’",  "Ã“",  "Ã”",   "Ã–",  "Ã˜",  "Ã™","Ãœk", "Ãœ","â€š",    "ks",   "kS",   "k",  "h",    "q",   "w",   "`",    "s",    "S", "a",    "Â¡",    "%",     "W",  "â€¢", "Â·", "âˆ™", "Â·", "~j",  "~", "\\","+"," à¤ƒ","^", "*",  "Ãž", "ÃŸ", "(", "Â¼", "Â½", "Â¿", "Ã€", "Â¾", "A", "-", "&", "&", "Å’", "]","~ ","@");
+
+var unicode_array = new Array("à¥°","QZ+","sa","a","à¤°à¥à¤¦à¥à¤§","Z","\"","\"","'","'", "à¥¦",  "à¥§",  "à¥¨",  "à¥©",     "à¥ª",   "à¥«",  "à¥¬",   "à¥­",   "à¥®",   "à¥¯",  "à¥žà¥",  "à¥˜",  "à¥™", "à¥™à¥",  "à¥š", "à¤œà¤¼à¥", "à¥›",  "à¥œ",  "à¥",   "à¥ž",  "à¥Ÿ",  "à¤±",  "à¤©",  "à¤¤à¥à¤¤", "à¤¤à¥à¤¤à¥", "à¤•à¥à¤¤",  "à¤¦à¥ƒ",  "à¤•à¥ƒ","à¤¨à¥à¤¨","à¤¨à¥à¤¨à¥","=k","f=", "à¤¹à¥à¤¨",  "à¤¹à¥à¤¯",  "à¤¹à¥ƒ",  "à¤¹à¥à¤®",  "à¤¹à¥à¤°",  "à¤¹à¥",   "à¤¦à¥à¤¦",  "à¤•à¥à¤·", "à¤•à¥à¤·à¥", "à¤¤à¥à¤°", "à¤¤à¥à¤°à¥",  "à¤›à¥à¤¯",  "à¤Ÿà¥à¤¯",  "à¤ à¥à¤¯",  "à¤¡à¥à¤¯",  "à¤¢à¥à¤¯", "à¤¦à¥à¤¯", "à¤œà¥à¤ž", "à¤¦à¥à¤µ", "à¤¶à¥à¤°",  "à¤Ÿà¥à¤°",    "à¤¡à¥à¤°",    "à¤¢à¥à¤°",    "à¤›à¥à¤°",   "à¤•à¥à¤°",  "à¤«à¥à¤°", "à¤°à¥à¤¦à¥à¤°",  "à¤¦à¥à¤°",   "à¤ªà¥à¤°", "à¤ªà¥à¤°",  "à¤—à¥à¤°", "à¤°à¥",  "à¤°à¥‚", "à¤‘",   "à¤“",  "à¤”",  "à¤†",   "à¤…", "à¤ˆà¤‚", "à¤ˆ",  "à¤ˆ",   "à¤‡",  "à¤‰",   "à¤Š",  "à¤",  "à¤", "à¤‹", "à¤•à¥à¤•", "à¤•", "à¤•", "à¤•à¥", "à¤–", "à¤–à¥", "à¤—", "à¤—", "à¤—à¥", "à¤˜", "à¤˜", "à¤˜à¥", "à¤™", "à¤šà¥ˆ",  "à¤š", "à¤š", "à¤šà¥", "à¤›", "à¤œ", "à¤œ", "à¤œà¥",  "à¤",  "à¤à¥", "à¤ž", "à¤Ÿà¥à¤Ÿ",   "à¤Ÿà¥à¤ ",   "à¤Ÿ",   "à¤ ",   "à¤¡à¥à¤¡",   "à¤¡à¥à¤¢",  "à¤¡à¤¼", "à¤¢à¤¼", "à¤¡",   "à¤¢", "à¤£", "à¤£à¥", "à¤¤", "à¤¤", "à¤¤à¥", "à¤¥", "à¤¥à¥",  "à¤¦à¥à¤§",  "à¤¦", "à¤§", "à¤§", "à¤§à¥", "à¤§à¥", "à¤§à¥", "à¤¨", "à¤¨", "à¤¨à¥",    "à¤ª", "à¤ª", "à¤ªà¥",  "à¤«", "à¤«à¥",  "à¤¬", "à¤¬", "à¤¬à¥",  "à¤­", "à¤­à¥",  "à¤®",  "à¤®", "à¤®à¥", "à¤¯", "à¤¯à¥",  "à¤°", "à¤²", "à¤²", "à¤²à¥",  "à¤³",  "à¤µ", "à¤µ", "à¤µà¥", "à¤¶", "à¤¶à¥",  "à¤·", "à¤·à¥", "à¤¸", "à¤¸", "à¤¸à¥", "à¤¹", "à¥€à¤‚", "à¥à¤°", "à¤¦à¥à¤¦", "à¤Ÿà¥à¤Ÿ","à¤Ÿà¥à¤ ","à¤¡à¥à¤¡","à¤•à¥ƒ","à¤­","à¥à¤¯","à¤¡à¥à¤¢","à¤à¥","à¤•à¥à¤°","à¤¤à¥à¤¤à¥","à¤¶","à¤¶à¥","à¥‰",  "à¥‹",   "à¥Œ",   "à¤¾",   "à¥€",   "à¥",   "à¥‚",   "à¥ƒ",   "à¥‡",   "à¥ˆ", "à¤‚",   "à¤",   "à¤ƒ",   "à¥…",  "à¤½", "à¤½", "à¤½", "à¤½", "à¥à¤°",  "à¥", "?", "à¤¼",":", "â€˜",   "â€™",   "â€œ",   "â€",  ";",  "(",    ")",   "{",    "}",   "=", "à¥¤", ".", "-",  "Âµ", "à¥°", ",","à¥ ","/");
+function krutiunicode()
+{
+
+var text_size = document.getElementById("krutitext").value.length ;
+var kruti_array_length = kruti_array.length ;
+var kruti_text = document.getElementById("krutitext").value  ;
+
+    var processed_text = '' ;
+
+    var n = 0 ;  var o = 0 ;  var r = 1 ;
+
+    var max_text_size = 7000;
+
+    while ( r == 1 )
+    {
+     n = o ;
+
+     if ( o < ( text_size - max_text_size ) )
+     {
+      o +=  max_text_size ;
+      while (document.getElementById("krutitext").value.charAt ( o ) != ' ') {o--;}
+     }
+     else  { o = text_size  ;  r = 0 }
+
+     var kruti_text = document.getElementById("krutitext").value.substring ( n, o )  ;
+
+    replsym( ) ;
+
+    processed_text += kruti_text ;
+
+
+    document.getElementById("unicodetext").value = processed_text  ;
+   }
+
+
+function replsym( )
+
+{
+
+
+if ( kruti_text != "" )
+for ( input_symbol_idx = 0;   input_symbol_idx < kruti_array_length;    input_symbol_idx++ )
+
+{
+
+idx = 0  ;
+
+while (idx != -1 )
+{
+
+kruti_text = kruti_text.replace( kruti_array[ input_symbol_idx ] , unicode_array[input_symbol_idx] )
+idx = kruti_text.indexOf( kruti_array[input_symbol_idx] )
+
+}
+}
+kruti_text = kruti_text.replace( /Â±/g , "Zà¤‚" ) ;
+
+
+kruti_text = kruti_text.replace( /Ã†/g , "à¤°à¥f" ) ;
+
+var pi = kruti_text.indexOf( "f" )
+
+while ( pi != -1 )
+{
+var cni = kruti_text.charAt( pi + 1 )
+var ctbr = "f" + cni
+kruti_text = kruti_text.replace( ctbr , cni + "à¤¿" )
+pi = kruti_text.search( /f/ , pi + 1 )
+
+}
+
+kruti_text = kruti_text.replace( /Ã‡/g , "fa" ) ;
+kruti_text = kruti_text.replace( /Ã‰/g , "à¤°à¥fa" ) ;
+
+var pi = kruti_text.indexOf( "fa" )
+
+while ( pi != -1 )
+{
+var cntip2 = kruti_text.charAt( pi + 2 )
+var ctbr = "fa" + cntip2
+kruti_text = kruti_text.replace( ctbr , cntip2 + "à¤¿à¤‚" )
+pi = kruti_text.search( /fa/ , pi + 2 )
+
+}
+
+kruti_text = kruti_text.replace( /ÃŠ/g , "à¥€Z" ) ;
+
+
+var powe = kruti_text.indexOf( "à¤¿à¥" )
+
+while ( powe != -1 )
+
+{
+var cntwe = kruti_text.charAt( powe + 2 )
+var ctbr = "à¤¿à¥" + cntwe
+kruti_text = kruti_text.replace( ctbr , "à¥" + cntwe + "à¤¿" )
+powe = kruti_text.search( /à¤¿à¥/ , powe + 2 )
+
+}
+matraslist = "à¤… à¤† à¤‡ à¤ˆ à¤‰ à¤Š à¤ à¤ à¤“ à¤” à¤¾ à¤¿ à¥€ à¥ à¥‚ à¥ƒ à¥‡ à¥ˆ à¥‹ à¥Œ à¤‚ : à¤ à¥…"
+var rpos = kruti_text.indexOf( "Z" )
+
+while ( rpos > 0 )
+{
+pphr = rpos - 1 ;
+var chtr = kruti_text.charAt( pphr )
+
+
+while ( matraslist.match( chtr ) != null )
+
+{
+pphr = pphr - 1 ;
+chtr = kruti_text.charAt( pphr ) ;
+
+}
+
+
+ctbr = kruti_text.substr ( pphr , ( rpos - pphr ) ) ;
+rstr = "à¤°à¥" + ctbr ;
+ctbr = ctbr + "Z" ;
+kruti_text = kruti_text.replace( ctbr , rstr ) ;
+rpos = kruti_text.indexOf( "Z" ) ;
+
+}
+
+}
+
+}
+function copyclipboard() {
+   var copyText = document.querySelector("#unicodetext");
+   copyText.select();
+   copyText.setSelectionRange(0, 99999);
+   document.execCommand("copy");
+   alert("Text Copied To Clipboard.");
+}
+function cleartext() {
+	document.getElementById("krutitext").value = '';
+	document.getElementById("unicodetext").value = '';
+}
